2f4abd18ac1e92ed4a17b221481db685791a369a
euclidesgc <euclides.catunda@gmail.com>
2023-07-30 01:08:35 -0300

    create a Prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhrasesModule } from './phrases/phrases.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PhrasesModule],
+  imports: [PhrasesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
