0366080afe55833243a19bd558a8aa33655ed8df
Euclides Catunda <euclides.catunda@grupoboticario.com.br>
2023-07-31 10:51:17 -0300

    add swagger in index.ts

--- index.ts
+++ index.ts
@@ -3,14 +3,26 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 const expressServer = express();
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-await app.init();
+
+  const config = new DocumentBuilder()
+    .setTitle('Frases que eu ouvi')
+    .setDescription('Frases que eu ouço no trabalho')
+    .setVersion('1.0')
+    .addTag('frases')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.init();
 };
+
 export const api = functions.https.onRequest(async (request, response) => {
   await createFunction(expressServer);
   expressServer(request, response);
